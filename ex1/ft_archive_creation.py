d373a7fd1bc41ec55b189b456ba31483bc11b6ad
Lahcen Sebar <lsebar@c3r3p4.1337.ma>
2026-01-15 21:25:07 +0100

    up

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,29 @@
+
+def main():
+	
+	print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
+
+	file_name = "new_discovery.txt"
+
+	print(f"Initializing new storage unit: {file_name}")
+	new_discovery = open(file_name, "w")
+	print("Storage unit created successfully...\n")
+	
+	print("Inscribing preservation data...")
+	new_discovery.write(
+		"[ENTRY 001] New quantum algorithm discovered\n"
+		"[ENTRY 002] Efficiency increased by 347%\n"
+		"[ENTRY 003] Archived by Data Archivist trainee")
+
+	print(
+		"[ENTRY 001] New quantum algorithm discovered\n"
+		"[ENTRY 002] Efficiency increased by 347%\n"
+		"[ENTRY 003] Archived by Data Archivist trainee\n")
+	
+	new_discovery.close()
+
+	print("Data inscription complete. Storage unit sealed.")
+	print("Archive 'new_discovery.txt' ready for long-term preservation.")
+
+
+main()
