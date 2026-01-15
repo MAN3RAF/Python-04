6741e4f60d9f417e20bcd90b4c9ba6d149415495
Lahcen Sebar <lsebar@c3r3p4.1337.ma>
2026-01-15 19:07:17 +0100

    up

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,24 @@
+
+def main():
+
+	print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n	")
+
+	try:
+		vault_name = "ancient_fragment.txt"
+		print(f"Accessing Storage Vault: {vault_name}")
+
+		vault = open(vault_name)
+
+		print("Connection established...\n")
+	
+		recovered_data = vault.read()
+
+		print(f"RECOVERED DATA:\n{recovered_data}")
+
+		vault.close()
+		print("\nData recovery complete. Storage unit disconnected.")
+	except:
+		print("\nERROR: Storage vault cannot be accessed due to coruption!")
+
+
+main()
